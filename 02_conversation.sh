018da5a1ac98f59f4675418fa5d4ffbaa248f6cf
Harika Eravathri <harikaeravathri@gmail.com>
2024-08-23 14:03:45 -0700

    I added conversation of 2 people

---
+++ 02_conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ram:: Hi Sam, how are you?"
+echo "Sam:: Hello Ram , I am  good, how are you doing?"
+echo "Ram:: I am doing good Sam, whats going on"
+echo "Sam:: I started learning devops"
