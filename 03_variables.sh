8f5e5c8f8c61b8d460c7dc8aef7c74dbfd6ac336
Harika Eravathri <harikaeravathri@gmail.com>
2024-08-23 14:26:57 -0700

    variables

---
+++ 03_variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Hi $PERSON2, how are you?"
+echo "$PERSON2:: Hello $PERSON1 , I am  good, how are you doing?"
+echo "$PERSON1:: I am doing good $PERSON2, whats going on"
+echo "$PERSON2:: I started learning devops"
