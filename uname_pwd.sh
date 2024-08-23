3a6c6962221397afff044c9f999b22b91087f583
Harika Eravathri <harikaeravathri@gmail.com>
2024-08-23 14:37:24 -0700

    username

--- uname_pwd.sh
+++ uname_pwd.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 echo "Please enter your username:"
-read USERNAME
+read -s USERNAME
 
 echo "username entered is: $USERNAME"
+
+echo "Please enter your password:"
+read -s PASSWORD
+
+echo "username entered is: $PASSWORD
