35945327f13c07e5aab7d761dd15f7b6c9b556fb
Harika Eravathri <harikaeravathri@gmail.com>
2024-08-26 13:17:29 -0700

    Installation

--- installation.sh
+++ installation.sh
@@ -12,8 +12,14 @@ dnf list installed mysql
 
 if [ $? -ne 0 ]
 then
-echo "mysql is not installed, going to install"
-dnf install mysql -y 
+  echo "mysql is not installed, going to install"
+   dnf install mysql -y 
+    if [ $? -ne 0 ]
+    then 
+            echo "mysql installation is failure"
+        exit1
+    else
+         echo "mysql installation is success"
 else
 echo "mysql is already installed"
 fi
