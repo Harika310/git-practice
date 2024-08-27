28a33ac1bd55d7f10637ca6311d60fa488f52126
Harika Eravathri <harikaeravathri@gmail.com>
2024-08-27 09:57:46 -0700

    redirectors

--- redirectors.sh
+++ redirectors.sh
@@ -2,48 +2,46 @@
 
 LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-TIME_STAMP=$(date +Y-%m-%d-%H-%M-%S)
-LOGS_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 mkdir -p $LOGS_FOLDER
 
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
-Y="\e[33m"
 N="\e[0m"
-
+Y="\e[33m"
 
 CHECK_ROOT(){
-
-if [ $USERID -ne 0 ]
-then
-echo -e "$R please run the script with root priveleges $N" &>>LOG_FILE
-exit 1
-fi
+    if [ $USERID -ne 0 ]
+    then
+        echo -e "$R Please run this script with root priveleges $N" &>>$LOG_FILE
+        exit 1
+    fi
 }
 
-VALIDATE()
-{
+VALIDATE(){
     if [ $1 -ne 0 ]
     then
-    echo -e "$2 is ...$R failed" &>>LOG_FILE
+        echo -e "$2 is...$R FAILED $N" &>>$LOG_FILE
+        exit 1
     else
-    echo -e "$2 is ...$G success" &>>LOG_FILE
+        echo -e "$2 is... $G SUCCESS $N" &>>$LOG_FILE
     fi
 }
 
 CHECK_ROOT
 
-# refers to all arguments passed to it
-for package in $@
+
+for package in $@ # $@ refers to all arguments passed to it
 do
-   dnf list installed $package &>>LOG_FILE
-   if [ $? -ne 0 ]
-   then
-   echo "$package is not istalled, going to install" &>>LOG_FILE
-   dnf install $package -y &>>LOG_FILE
-   VALIDATE $? "Installing $package"
-   else
-   echo -e "$Y $package is already installed" &>>LOG_FILE
-   fi
-done
+    dnf list installed $package &>>$LOG_FILE
+    if [ $? -ne 0 ]
+    then
+        echo "$package is not installed, going to install it.." &>>$LOG_FILE              
+        dnf install $package -y &>>$LOG_FILE
+        VALIDATE $? "Installing $package"
+    else
+        echo -e "$package is already $Y installed..nothing to do $N" &>>$LOG_FILE
+    fi
+done    
