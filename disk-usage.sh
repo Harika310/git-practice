669f7201d49fc86c179a793d0603828c5e014b8b
Harika Eravathri <harikaeravathri@gmail.com>
2024-08-29 16:33:14 -0700

    disk usage

--- disk-usage.sh
+++ disk-usage.sh
@@ -6,7 +6,7 @@ DISK_THRESHOLD=5 #real projects, it is usually 75
 
 while IFS= read -r line #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
 do
-    USAGE=$(echo $line | grep xfs | awk -F " " '{print $6F}')
+    USAGE=$(echo $line | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
     PARTITION=$(echo $line | grep xfs | awk -F " " '{print $NF}')
     if [ $USAGE -ge $DISK_THRESHOLD ]
     then
