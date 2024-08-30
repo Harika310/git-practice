b8271c919994bdf2f2e425c31e2fefd6ea2d9abe
Harika Eravathri <harikaeravathri@gmail.com>
2024-08-30 14:50:07 -0700

    task

--- 01-repeated_words.sh
+++ 01-repeated_words.sh
@@ -19,6 +19,6 @@ else
         echo "File Doesn't exit"
 fi
 
-count=$(cat $FILEPATH | grep  '[[:alnum:]]\+' -wo abc.txt | wc -l | sort | uniq -c)
+count=$(cat $FILEPATH | grep  '[[:alnum:]]\+' -wo $FILEPATH | wc -l | sort | uniq -c)
 echo "$SEARCH_WORD"=" $count"
 
