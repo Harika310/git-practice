00bfa57b0aab05ddb09a2fcfb8098622fb7c26bb
Harika Eravathri <harikaeravathri@gmail.com>
2024-08-30 14:54:54 -0700

    task

--- 01-repeated_words.sh
+++ 01-repeated_words.sh
@@ -19,6 +19,6 @@ else
         echo "File Doesn't exit"
 fi
 
-count=$(cat $FILEPATH | grep  '[[:alnum:]]\+' -wo $FILEPATH | wc -l | uniq -c)
+count=$(cat $FILEPATH | cut -d" " -f1 | wc -l | uniq -c)
 echo "$SEARCH_WORD"=" $count"
 
