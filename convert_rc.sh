bc03b896594af55dd3b09913b29fa5ba78b06fdd
Harika Eravathri <harikaeravathri@gmail.com>
2024-08-30 15:40:40 -0700

    task

---
+++ convert_rc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+FILEPATH=$1
+
+USAGE(){
+    echo -e "$R USAGE:: $N 01-repeated_words.sh <filepath>"
+}
+
+if [ $# -eq 0 ]  # check the no of arguments
+then 
+   USAGE
+fi
+
+if [ -f $FILEPATH ]
+then
+        echo "File exist"
+else
+        echo "File Doesn't exit"
+fi
