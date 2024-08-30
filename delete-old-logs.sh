75a7c78f91280fce823003c290b781f9bf30dbb8
Harika Eravathri <harikaeravathri@gmail.com>
2024-08-30 12:05:24 -0700

    task

--- delete-old-logs.sh
+++ delete-old-logs.sh
@@ -9,9 +9,3 @@ Y="\e[33m"
 FILES=$(find $SOURCE_DIR -name "*.txt")
 echo "Files: $FILES"
 
-# dont use line, it is reserverd word
-while IFS= read -r file #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
-# do
-#     echo "Deleting file: $file"
-#     rm -rf $file
-# done <<< $FILES
