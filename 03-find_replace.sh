0c900de77bb718dbab6f8eb1a9a0d1f74e7b6ca8
Harika Eravathri <harikaeravathri@gmail.com>
2024-08-30 21:49:34 -0700

    task

--- 03-find_replace.sh
+++ 03-find_replace.sh
@@ -7,12 +7,19 @@ USAGE(){
     echo -e "$R USAGE:: $N 01-repeated_words.sh <filepath1> <filepath2>"
 }
 
-if [ $# eq 0 ]  # check the no of arguments
+if [ $# -eq 0 ]  # check the no of arguments
 then 
    USAGE
 fi
 
-if [ -f $FILEPATH ]
+if [ -f $FILEPATH1 ]
+then
+        echo "File exist"
+else
+        echo "File Doesn't exit"
+fi
+
+if [ -f $FILEPATH 2]
 then
         echo "File exist"
 else
