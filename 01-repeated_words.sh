f474ab6f255c9c730bd1bd07dba8a0c9ee3e7102
Harika Eravathri <harikaeravathri@gmail.com>
2024-08-30 21:29:23 -0700

    task

--- 01-repeated_words.sh
+++ 01-repeated_words.sh
@@ -23,16 +23,8 @@ fi
 #     echo "$line"
 # done < "$FILEPATH"
 
-#count=$(cat "$FILEPATH" | tr -s ' ' '\n' | sort )
-#repeated_words=$(echo "$count" | uniq -c)
-#echo "$repeated_words"
-
-word_count=$(tr -s '[:space:]' '\n' < "FILEPATH" | tr -d '[:punct:]' | tr '[:upper:]' '[:lower:]' | grep -v '^$' | sort | uniq -c | sort -nr)
-echo "Top 5 Most Frequent Words:"
-echo "$word_count" | head -n 5 | awk '{print $2 " - " $1 " occurrences"}'
-
-
-
-
+count=$(cat "$FILEPATH" | tr -s ' ' '\n' | sort )
+repeated_words=$(echo "$count" | uniq -c)
+echo "$repeated_words"
 
 
