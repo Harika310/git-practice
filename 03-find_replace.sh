b8de5710c9404bc559f414f3cd529b9ff75e1837
Harika Eravathri <harikaeravathri@gmail.com>
2024-08-30 22:01:49 -0700

    task

--- 03-find_replace.sh
+++ 03-find_replace.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 FILEPATH1=$1
-FILEPATH2=$2
+
 
 USAGE(){
-    echo -e "$R USAGE:: $N 01-repeated_words.sh <filepath1> <filepath2>"
+    echo -e "$R USAGE:: $N 01-repeated_words.sh <filepath1>"
 }
 
 if [ $# -eq 0 ]  # check the no of arguments
@@ -19,14 +19,7 @@ else
         echo "File Doesn't exit"
 fi
 
-if [ -f $FILEPATH2 ]
-then
-        echo "File exist"
-else
-        echo "File Doesn't exit"
-fi
 
-while IFS= read -r line
-do
-    echo "$line"
-done < "$FILEPATH1"
+FILES=$(find $FILEPATH1 -name "*.txt" -mtime +14)
+
+echo "Files: $FILES"
