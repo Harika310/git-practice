077103201a1e4de47fce4117ff30fbd12865098b
Harika Eravathri <harikaeravathri@gmail.com>
2024-09-01 14:11:57 -0700

    task

--- 03-find_replace.sh
+++ 03-find_replace.sh
@@ -6,20 +6,19 @@ G="\e[32m"
 N="\e[0m"
 Y="\e[33m"
 
-if [ -d $SOURCE_DIR ]
-then
-    echo -e "$SOURCE_DIR $G Exists $N"
-else
-    echo -e "$SOURCE_DIR $R does not exist $N"
-    exit 1
-fi
+# if [ -d $SOURCE_DIR ]
+# then
+#     echo -e "$SOURCE_DIR $G Exists $N"
+# else
+#     echo -e "$SOURCE_DIR $R does not exist $N"
+#     exit 1
+# fi
 
 FILES=$(find ${SOURCE_DIR} -name "*.txt")
 echo "Files: $FILES"
 
 read -p "Enter the search string:"
 read -p "Enter the replace string:"
-
 for f in Files
 do
 echo "Processing $f.."
