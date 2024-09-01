2b88940e2617114139f51d62970bb743a015073f
Harika Eravathri <harikaeravathri@gmail.com>
2024-08-31 19:49:25 -0700

    task

--- 03-abc.sh
+++ 03-abc.sh
@@ -1,33 +1,25 @@
 #!/bin/bash
 
-# FILEPATH=$1
-
-# # USAGE(){
-# #     echo -e "$R USAGE:: $N 02-convert_rc.sh <filepath>"
-# # }
-
-# # if [ $# -eq 0 ]  # check the no of arguments
-# # then 
-# #    USAGE
-# # fi
-
-# if [ -f $FILEPATH ]
-# then
-#         echo "File exist"
-# else
-#         echo "File Doesn't exit"
-#         exit 1
-# fi
+FILEPATH=$/home/ec2-user/abc.txt
 
 
-echo -e "Please enter your data\n"
+if [ -f $FILEPATH ]
+then
+        echo "File exist"
+else
+        echo "File Doesn't exit"
+        exit 1
+fi
+
+
+echo  "Please enter your data"
 read  DATA
 
-echo "Before data: $DATA"
+echo "Before data: $DATA" >abc.txt
 
 echo "After converting data" 
-cols=$(head -2 "$DATA" | wc -w)
+cols=$(head -2 "$FILEPATH" | wc -w)
 for i in $(seq 1 "$cols")
 do
-    echo $(cut -d ' ' -f "$i" "$DATA")
+    echo $(cut -d ' ' -f "$i" "$FILEPATH")
 done
