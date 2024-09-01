f3c90b27cdc732fe950fe30a51c583b5e210064e
Harika Eravathri <harikaeravathri@gmail.com>
2024-08-31 19:41:05 -0700

    task

--- 02-convert_rc.sh
+++ 02-convert_rc.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-FILEPATH=/home/ec2-user/abc.txt
+FILEPATH=$1
 
+USAGE(){
+    echo -e "$R USAGE:: $N 02-convert_rc.sh <filepath>"
+}
+
+if [ $# -eq 0 ]  # check the no of arguments
+then 
+   USAGE
+fi
 if [ -f $FILEPATH ]
 then
         echo "File exist"
@@ -9,14 +17,7 @@ else
         echo "File Doesn't exit"
         exit 1
 fi
-
-
-echo "Please enter your data:"
-read  DATA
-
-echo "Before data is: $DATA"
-
-#  echo "After converting data" 
+ 
 cols=$(head -2 "$FILEPATH" | wc -w)
 for i in $(seq 1 "$cols")
 do
---
+++ 03-abc.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# FILEPATH=$1
+
+# # USAGE(){
+# #     echo -e "$R USAGE:: $N 02-convert_rc.sh <filepath>"
+# # }
+
+# # if [ $# -eq 0 ]  # check the no of arguments
+# # then 
+# #    USAGE
+# # fi
+
+# if [ -f $FILEPATH ]
+# then
+#         echo "File exist"
+# else
+#         echo "File Doesn't exit"
+#         exit 1
+# fi
+
+
+echo "Please enter your data: \n"
+read  DATA
+
+echo "Before data: $DATA"
+
+echo "After converting data" 
+cols=$(head -2 "$DATA" | wc -w)
+for i in $(seq 1 "$cols")
+do
+    echo $(cut -d ' ' -f "$i" "$DATA")
+done
