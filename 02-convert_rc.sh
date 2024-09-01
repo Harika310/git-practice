dc8fa76a6eca9fc4bf5229f8af8c80b8562b963d
Harika Eravathri <harikaeravathri@gmail.com>
2024-08-31 19:34:07 -0700

    task

--- 02-convert_rc.sh
+++ 02-convert_rc.sh
@@ -2,15 +2,6 @@
 
 FILEPATH=/home/ec2-user/abc.txt
 
-# USAGE(){
-#     echo -e "$R USAGE:: $N 02-convert_rc.sh <filepath>"
-# }
-
-# if [ $# -eq 0 ]  # check the no of arguments
-# then 
-#    USAGE
-# fi
-
 if [ -f $FILEPATH ]
 then
         echo "File exist"
@@ -25,8 +16,8 @@ read  DATA
 
 echo "Before data is: $DATA"
 
- echo "After converting data" 
-cols=$(head -2 "$FILEPATH" | wc -l)
+#  echo "After converting data" 
+cols=$(head -2 "$FILEPATH" | wc -w)
 for i in $(seq 1 "$cols")
 do
     echo $(cut -d ' ' -f "$i" "$FILEPATH")
