6c129e98e821ea1fc93ac741ed6848668fe4f919
Harika Eravathri <harikaeravathri@gmail.com>
2024-09-01 16:09:41 -0700

    task

--- 03-find_replace.sh
+++ 03-find_replace.sh
@@ -6,21 +6,26 @@ G="\e[32m"
 N="\e[0m"
 Y="\e[33m"
 
-FILES=$(find ${SOURCE_DIR} -name "*.txt" | cat "*.txt")
+FILES=$(find ${SOURCE_DIR} -name "*.txt")
 echo "Files: $FILES"
+
+for files in "$SOURCE_DIR"/*
+do
+  echo "$files"
+done
 
 echo "files before replacement"
 
-while read -r file
-do
-  echo "$file"
-done <<< $FILES
+# while read -r file
+# do
+#   echo "$file"
+# done <<< $FILES
 
 echo "Replacing a word anvesh with harika"
 echo "files after replacement"
 for f in Files
 do
-grep 'anvesh' | xargs sed 's/anvesh/harika/g' "$f"
+grep '' | xargs sed 's/anvesh/harika/g' "$f"
 echo "Replacement done"
 done
 
