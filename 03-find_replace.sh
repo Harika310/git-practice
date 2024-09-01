5852d6a64b4c8c20c1e393536abc3c06064ee344
Harika Eravathri <harikaeravathri@gmail.com>
2024-09-01 15:20:21 -0700

    task

--- 03-find_replace.sh
+++ 03-find_replace.sh
@@ -9,15 +9,16 @@ Y="\e[33m"
 FILES=$(find ${SOURCE_DIR} -name "*.txt")
 echo "Files: $FILES"
 
-while read -r file
-do
-  echo "$file"
-done <<< $FILES
+read -p "$FILES"
+
+# while read -r file
+# do
+#   echo "$file"
+# done <<< $FILES
 
 read -p "Enter the search string:"
 read -p "Enter the replace string:"
 for f in Files
 do
-echo "Processing $f.."
 grep 'ram' | sed -i 's/ram/anvesh/g' "$f"
 done
