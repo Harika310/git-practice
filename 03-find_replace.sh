0ec81c86cee93e6116bb2541386ffd1966ae2fe6
Harika Eravathri <harikaeravathri@gmail.com>
2024-09-01 11:40:18 -0700

    task

--- 03-find_replace.sh
+++ 03-find_replace.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-FILEPATH1=$1
+FILEPATH1=/home/ec2-user/logs
 
 
-USAGE(){
-    echo -e "$R USAGE:: $N 03-find_replace.sh <filepath1>"
-}
+# USAGE(){
+#     echo -e "$R USAGE:: $N 03-find_replace.sh <filepath1>"
+# }
 
-if [ $# -eq 0 ]  # check the no of arguments
-then 
-   USAGE
-fi
+# if [ $# -eq 0 ]  # check the no of arguments
+# then 
+#    USAGE
+# fi
 
 if [ -f $FILEPATH1 ]
 then
