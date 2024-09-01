3ef9cab9636a4377d431b391b1f77fb2bf2971c1
Harika Eravathri <harikaeravathri@gmail.com>
2024-09-01 11:35:10 -0700

    task

--- 03-find_replace.sh
+++ 03-find_replace.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-FILEPATH1=/home/ec2-user
+FILEPATH1=$1
 
 
-# USAGE(){
-#     echo -e "$R USAGE:: $N 01-repeated_words.sh <filepath1>"
-# }
+USAGE(){
+    echo -e "$R USAGE:: $N 03-find_replace.sh <filepath1>"
+}
 
-# if [ $# -lt 2 ]
-# then
-#     USAGE
-#     exit 1
-# fi
+if [ $# -lt 2 ]
+then
+    USAGE
+    exit 1
+fi
 
 if [ -f $FILEPATH1 ]
 then
@@ -20,7 +20,7 @@ else
         echo "File Doesn't exit"
 fi
 
-
-FILES=$(find $FILEPATH1 -name "*.txt" -mtime +14)
-
-echo "Files: $FILES"
+for file in $FILEPATH/*.txt
+ do
+    sed -i 's/ram/anvesh/g' "$file"
+done
