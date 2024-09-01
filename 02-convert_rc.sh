6c6447a70361cbbe82192ff1a6aa9e5a96b7c0c8
Harika Eravathri <harikaeravathri@gmail.com>
2024-08-31 19:19:56 -0700

    task

--- 02-convert_rc.sh
+++ 02-convert_rc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FILEPATH=$1
+FILEPATH=/home/ec2-user/abc.txt
 
 USAGE(){
     echo -e "$R USAGE:: $N 02-convert_rc.sh <filepath>"
@@ -11,14 +11,19 @@ then
    USAGE
 fi
 
-if [ -f $FILEPATH ]
-then
-        echo "File exist"
-else
-        echo "File Doesn't exit"
-fi
+# if [ -f $FILEPATH ]
+# then
+#         echo "File exist"
+# else
+#         echo "File Doesn't exit"
+# fi
+echo "Please enter your data:"
+read  DATA
 
-cols=$(head -2 "$FILEPATH" | wc -l)
+echo "data entered is: $DATA"
+
+ echo "After converting data" 
+cols=$ead  DATA(head -2 "$FILEPATH" | wc -l)
 for i in $(seq 1 "$cols")
 do
     echo $(cut -d ' ' -f "$i" "$FILEPATH")
