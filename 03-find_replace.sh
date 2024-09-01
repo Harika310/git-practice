de1994c95701fffe1bdba801f9689be3ecac5bc6
Harika Eravathri <harikaeravathri@gmail.com>
2024-09-01 15:01:41 -0700

    task

--- 03-find_replace.sh
+++ 03-find_replace.sh
@@ -6,14 +6,6 @@ G="\e[32m"
 N="\e[0m"
 Y="\e[33m"
 
-# if [ -d $SOURCE_DIR ]
-# then
-#     echo -e "$SOURCE_DIR $G Exists $N"
-# else
-#     echo -e "$SOURCE_DIR $R does not exist $N"
-#     exit 1
-# fi
-
 FILES=$(find ${SOURCE_DIR} -name "*.txt")
 echo "Files: $FILES"
 
@@ -22,10 +14,10 @@ do
   echo "$file"
 done < $SOURCE_DIR
 
-read -p "Enter the search string:"
-read -p "Enter the replace string:"
-for f in Files
-do
-echo "Processing $f.."
-grep 'ram' | sed -i 's/ram/anvesh/g' "$f"
-done
+# read -p "Enter the search string:"
+# read -p "Enter the replace string:"
+# for f in Files
+# do
+# echo "Processing $f.."
+# grep 'ram' | sed -i 's/ram/anvesh/g' "$f"
+# done
